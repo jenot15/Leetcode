eb023031fcc0fac353c823af8b318dabf7780129
jenot15 <kaczorek.kamila@gmail.com>
2023-11-02 16:20:38 +0100

    Minimum_Value_to_Get_Positive_Step_by_Step_Sum

---   Running_Sum_of_1d_Array/solution1.py
+++   Running_Sum_of_1d_Array/solution1.py
@@ -1,8 +1,15 @@
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
+    def minStartValue(self, nums: List[int]) -> int:
         prefix = [nums[0]]
         for i in range(1, len(nums)):
             prefix.append(nums[i]+prefix[-1])
+        
+        minimum = min(prefix)
+        #print(minimum, abs(minimum - 1))
+        
+        if minimum == 1:
+            return 1
+        
+        return abs(minimum - 1)
             
-        return prefix
         
